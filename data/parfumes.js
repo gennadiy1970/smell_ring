778ccd9eb8202e541320cb118024124f9bf6c88f
Gennadiy Gorbulin <gennadiy.gorbulin@yahoo.com>
2018-04-11 23:27:46 +0300

    add data files

---
+++ data/parfumes.js
@@ -0,0 +1,723 @@
+const parfumes = {
+  "0": [
+    {
+      brand: "Lulu Castagnette",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Roberto Verino",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Lanvin",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Loewe",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "LR",
+      name: "Pseudonym",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    }
+  ],
+  "1": [
+    {
+      brand: "LR",
+      name: "Beauty Queen ",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Sergio Tacchini",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    }
+  ],
+  "2": [
+    {
+      brand: "LR",
+      name: "Eau de Style",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: "w"
+    },
+    {
+      brand: "Lanvin",
+      name: "Eclat d`Arpege",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: "w"
+    },
+    {
+      brand: "La Perla",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: "w"
+    },
+    {
+      brand: "DKNY",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: "w"
+    },
+    {
+      brand: "Versace",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: "w"
+    }
+  ],
+  "3": [
+    {
+      brand: "LR",
+      name: "Heart & Soul",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Loewe",
+      name: "I Loewe You Tonight",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Nina Ricci",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Jimmy Choo",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Beyonce",
+      name: "Heat",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Adidas",
+      name: "Fruity Rhythm",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Escada",
+      name: "Marine Groove",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Bruno",
+      name: "Banani Pure Women",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "D&G",
+      name: "L'Imperatrice 3",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Gianfranco Ferre",
+      name: "Rose Princesse",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Guerlain",
+      name: "Aqua Allegoria Grosellina",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Lacoste",
+      name: "Touch of Pink",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    }
+  ],
+  "4": [
+    {
+      brand: "LR",
+      name: "Femme Noblesse",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Chanel",
+      name: "#16",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Guerlain",
+      name: "Idylle Eau de Parfum",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    }
+  ],
+  "5": [
+    {
+      brand: "LR",
+      name: "Harem",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Yves Saint Laurent",
+      name: "Opium",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Lancome",
+      name: "Magie Noire",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Fendi",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Eisenberg",
+      name: "J'ose",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Christian Dior",
+      name: "Hypnotic Poison",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Van Cleef & Arpels",
+      name: "Oriens",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Agent Provocateur",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    }
+  ],
+  "6": [
+    {
+      brand: "LR",
+      name: "Sensual Grace",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Lancome",
+      name: "Hypnose",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Armand Basi",
+      name: "in Red",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Guerlain",
+      name: "My Insolence",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Burberry",
+      name: "Women Burberry",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Paco Rabanne",
+      name: "Lady Million",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Fendi",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    }
+  ],
+  "7": [
+    {
+      brand: "LR",
+      name: "Rockin' Romance",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "D&G",
+      name: "Light Blue",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Christian Dior",
+      name: "Addict 2",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Chanel",
+      name: "Chance Eau Fraince",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    }
+  ],
+  "8": [
+    {
+      brand: "LR",
+      name: "Los Angeles",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "La Perla",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "DKNY",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Emilio Pucci",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    }
+  ],
+  "9": [
+    {
+      brand: "LR",
+      name: "Valencia",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Givenchy",
+      name: "Ange ou Demon",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Cerruti",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Blumarine",
+      name: "Bellissima",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Chanel",
+      name: "Coco Mademoiselle",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Clinique",
+      name: "Happy",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    }
+  ],
+  "10": [
+    {
+      brand: "LR",
+      name: "Lovingly by Bruce Willis",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Lanvin",
+      name: "Marry Me",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Calvin Klein",
+      name: "Beauty",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    }
+  ],
+  "11": [
+    {
+      brand: "LR",
+      name: "Bruce Willis",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Angle Schlesser",
+      name: "Essential for Men",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "DSQUARED²",
+      name: "Wood",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "D&G",
+      name: "The One for Men",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    },
+    {
+      brand: "Montblanc",
+      name: "",
+      type: [],
+      top_note: [],
+      middle_note: [],
+      base_note: [],
+      sex: ""
+    }
+  ],
+  "12": [
+    {
+      brand: "LR",
+      name: "Metropoliten Men",
+      type: ["чувственно-элегантный"],
+      top_note: ["грейпфрут", "перец", "бергамот"],
+      middle_note: ["гуаяк", "кардамон", "герань"],
+      base_note: ["ветивер", "кедр (белый)", "амбра", "шоколад"],
+      sex: "m"
+    },
+    {
+      brand: "Giorgio Armani",
+      name: "Code",
+      type: ["пряный", "восточный"],
+      top_note: ["бергамот", "лимон"],
+      middle_note: ["анис (звездчатый)", "цветок оливкового дерева", "гуаяк"],
+      base_note: ["кожа", "табак", "бобы тонка"],
+      sex: "m"
+    },
+    {
+      brand: "Calvin Klein",
+      name: "Euphoria Men",
+      type: ["древесный", "фужерный"],
+      top_note: ["имбирь", "перец"],
+      middle_note: ["шалфей", "кедр (белый)", "базилик (черный)"],
+      base_note: ["амбра", "пачули", "замша", "махагони (бразильский)"],
+      sex: "m"
+    }
+  ],
+  "13": [
+    {
+      brand: "LR",
+      name: "Just Sport Racing",
+      type: ["чувственно-элегантный", "древесный", "мускусный"],
+      top_note: ["анис", "имбирь", "ананас"],
+      middle_note: ["перец", "мускатный орех", "кардамон"],
+      base_note: ["ветивер", "палисандр", "мускус"],
+      sex: "m"
+    },
+    {
+      brand: "Christian Dior",
+      name: "Homme Sport",
+      type: ["древесный", "фужерный"],
+      top_note: ["грейпфрут", "амальфитанский лимон", "элеми", "бергамот"],
+      middle_note: ["ветивер", "имбирь", "кедр (вирджиния)"],
+      base_note: ["сандал", "розмарин", "лаванда"],
+      sex: "m"
+    },
+    {
+      brand: "Lanvin",
+      name: "L`Homme",
+      type: ["цветочный", "древесный", "мускусный"],
+      top_note: ["лаванда", "мандарин", "нероли", "бергамот"],
+      middle_note: ["перец", "мята", "шалфей", "кардамон"],
+      base_note: ["сандал", "амбра", "мускус", "ваниль"],
+      sex: "m"
+    },
+    {
+      brand: "Davidoff",
+      name: "Champion",
+      type: ["древесный", "фужерный"],
+      top_note: ["лимон", "бергамот"],
+      middle_note: ["гальбанум", "мускатный орех", "шалфей"],
+      base_note: ["мох (дубовый)", "кедр (белый)"],
+      sex: "m"
+    },
+    {
+      brand: "Guerlain",
+      name: "Homme",
+      type: ["древесный", "фужерный"],
+      top_note: ["лайм", "мята", "бергамот"],
+      middle_note: ["ветивер", "герань", "мохито"],
+      base_note: ["ром", "кедр (белый)"],
+      sex: "m"
+    },
+    {
+      brand: "Ermenegildo Zegna",
+      name: "Z Zegna",
+      type: ["пряный", "древесный"],
+      top_note: ["бергамот (сицилия)", "розмарин"],
+      middle_note: ["мускатный орех", "белый перец", "ирис"],
+      base_note: ["мускус", "мох (дубовый)", "пачули", "кашмирское дерево"],
+      sex: "m"
+    },
+    {
+      brand: "Calvin Klein",
+      name: "Eternity for Men",
+      type: ["фужерный"],
+      top_note: ["лаванда", "мандарин", "лимон", "бергамот"],
+      middle_note: [
+        "кориандр",
+        "лилия",
+        "ягоды можжевельника",
+        "апельсиновый цвет",
+        "базилик",
+        "жасмин",
+        "шалфей",
+        "ландыш",
+        "герань"
+      ],
+      base_note: ["розовое дерево", "ветивер", "мускус", "амбра", "сандал"],
+      sex: "m"
+    },
+    {
+      brand: "Davidoff",
+      name: "Cool Water",
+      type: ["водяной", "фужерный"],
+      top_note: [
+        "морская вода",
+        "мята",
+        "зелень",
+        "лаванда",
+        "кориандр",
+        "розмарин",
+        "калон"
+      ],
+      middle_note: ["сандал", "жасмин", "нероли", "герань"],
+      base_note: ["мускус", "мох (дубовый)", "кедр (белый)", "амбра", "табак"],
+      sex: "m"
+    }
+  ]
+};
